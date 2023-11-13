b7389c6f32898291dae77c880260b8482bf8d713
Arthur <liege.arthur60@gmail.com>
2023-11-13 16:49:45 +0100

    chore: add pino to handle logging

--- functions/modules/user/validation.js
+++ functions/modules/user/validation.js
@@ -1,3 +1,10 @@
-exports.validateUser = function (uid) {
-    return typeof uid !== 'undefined' && uid
+const pino = require("../../logger-setup");
+
+exports.validateUser = function (userObject) {
+    pino.debug('Check if UID is valid')
+    const valid = typeof userObject.uid !== 'undefined' && userObject.uid
+    if(!valid){
+        pino.error('UID for this user is invalid: '+JSON.stringify(userObject) )
+    }
+    return valid
 }
