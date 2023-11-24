e345621606c3f1bae5204f83696960f40f0c0f44
Jdevvv <llanta.jeremy@gmail.com>
2023-11-24 10:33:57 +0100

    :bug: Fix missing user validation file

---
+++ functions/modules/users/validation.js
@@ -0,0 +1,12 @@
+const logger = require('../../logger-setup');
+
+exports.validateUser = (userObject) => {
+    logger.debug('Check if UID is valid');
+    const valid = typeof userObject.uid !== 'undefined' && userObject.uid;
+    if (!valid) {
+        logger.error(
+            'UID for this user is invalid: ' + JSON.stringify(userObject)
+        );
+    }
+    return valid;
+};
