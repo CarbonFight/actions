28217aa5531f4572d68754cb0525b7b0a8eff349
Jdevvv <llanta.jeremy@gmail.com>
2023-11-30 15:04:25 +0100

    :fire: Remove non-periodic users action condition

--- functions/modules/actions/methods/get-actions-by-user.js
+++
@@ -1,3 +0,0 @@
-module.exports.getUserActions = async function (db, uid) {
-    return await db.collection('actions').where('uid', '==', uid).get();
-};
--- functions/modules/crons/methods/resetDailyStats.js
+++ functions/modules/crons/methods/resetDailyStats.js
@@ -3,7 +3,6 @@ const Logger = require('../../../logger-setup');
 const { dbInstance } = require('../../../db-setup');
 
 const { getUsers } = require('../../users/methods/get-users');
-const { getUserActions } = require('../../actions/methods/get-actions-by-user');
 const { resetDailyStats } = require('../../stats/methods/reset-daily-stats');
 
 module.exports.resetDailyStats = async function () {
@@ -16,17 +15,7 @@ module.exports.resetDailyStats = async function () {
             const users = usersSnapshot.docs.map((doc) => doc.data().uid);
 
             for (const userId of users) {
-                const actionsSnapshot = await getUserActions(db, userId);
-
-                // Check if the user has only non-periodic actions
-                const hasOnlyNonPeriodicActions = actionsSnapshot.docs.every(
-                    (doc) => !doc.data().isPeriodic
-                );
-
-                // If no periodic action, reset daily stats
-                if (hasOnlyNonPeriodicActions) {
-                    await resetDailyStats(db, userId);
-                }
+                await resetDailyStats(db, userId);
             }
 
             Logger.info('Daily stats have been successfully reset.');
