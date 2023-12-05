8ecaf9606388d272a4e739b3d8d544c45d7a4a0d
Jdevvv <llanta.jeremy@gmail.com>
2023-12-05 13:48:22 +0100

    :pencil2: Test fix CI

--- functions/db-setup.js
+++ functions/db-setup.js
@@ -1,45 +1,47 @@
-const admin = require('firebase-admin')
-const firebase = require("@firebase/testing");
-const { readFileSync } = require("fs");
-const { FieldValue } = require("firebase-admin/firestore");
+const admin = require('firebase-admin');
+const firebase = require('@firebase/testing');
+const { readFileSync } = require('fs');
+const { FieldValue } = require('firebase-admin/firestore');
 
-const projectId = "carbonfight-test-" + new Date().getTime()
-async function dbInstanceSetup(){
-    let db = null
+const projectId = 'carbonfight-test-' + new Date().getTime();
+async function dbInstanceSetup() {
+    let db = null;
 
-    if(process.env.NODE_ENV === 'test'){
-        if(firebase.apps.length){
-            db = firebase.apps[0]
+    if (process.env.NODE_ENV === 'test') {
+        if (firebase.apps.length) {
+            db = firebase.apps[0];
         } else {
-            process.env.GCLOUD_PROJECT = projectId
-            process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+            process.env.GCLOUD_PROJECT = projectId;
+            process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8080';
 
             const app = await firebase.initializeTestApp({
-                projectId
+                projectId,
             });
 
             db = app.firestore();
 
             await firebase.loadFirestoreRules({
                 projectId,
-                rules: readFileSync('../firestore.rules', 'utf8')
+                rules: readFileSync('../firestore.rules', 'utf8'),
             });
         }
     } else {
         try {
-            admin.initializeApp()
-            db = admin.firestore()
-            db.settings({ignoreUndefinedProperties: true})
-        }
-        catch(err){
-            admin.app()
-            db = admin.firestore()
+            admin.initializeApp();
+            db = admin.firestore();
+            db.settings({ ignoreUndefinedProperties: true });
+        } catch (err) {
+            admin.app();
+            db = admin.firestore();
         }
     }
 
-    return db
+    return db;
 }
 
-module.exports.dbInstance = dbInstanceSetup
-module.exports.projectId = projectId
-module.exports.fieldValue = process.env.NODE_ENV === 'test' ? firebase.firestore.FieldValue : FieldValue;
+module.exports.dbInstance = dbInstanceSetup;
+module.exports.projectId = projectId;
+module.exports.fieldValue =
+    process.env.NODE_ENV === 'test'
+        ? firebase.firestore.FieldValue
+        : FieldValue;
