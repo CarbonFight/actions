eca05c0c0f85cb381008ab3dbc298f478e49ac44
Pierre Freund <pierre.freund@gmail.com>
2024-03-04 22:47:58 +0100

    Landing V2

--- landing/js/index.js
+++ landing/js/index.js
@@ -41,58 +41,15 @@ function scrollToTop() {
         divElement.classList.toggle("visible"); // Toggle entre ajouter ou supprimer la classe visible
     });
 
-    
-  //COOKIES
-  // Vérifie si le cookie d'acceptation existe
-  function checkCookie() {
-    var accepted = getCookie("cookiesAccepted");
-    if (accepted) {
-      // Si le cookie existe, masquer la boîte de dialogue
-      document.querySelector('.container').style.display = 'none';
-    }
-  }
+  
 
-  // Fonction pour récupérer un cookie par son nom
-  function getCookie(name) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.startsWith(name + '=')) {
-        return cookie.substring(name.length + 1);
-      }
-    }
-    return null;
-  }
-
-  // Fonction pour définir un cookie avec une durée d'expiration
-  function setCookie(name, value, days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }
-  // Vérifier si le cookie d'acceptation existe lors du chargement de la page
-  window.onload = checkCookie;
-  // Sélectionnez le bouton "close"
-  var closeButton = document.querySelector('.close');
-
-  // Ajoutez un gestionnaire d'événements pour le clic sur le bouton "close"
-  closeButton.addEventListener('click', function() {
-    // Sélectionnez le conteneur parent
-    var container = document.querySelector('.container');
-    // Masquez le conteneur parent
-    container.style.display = 'none';
-  });
-
-  // Sélectionnez le bouton "accept"
-  var acceptButton = document.querySelector('.accept');
-
-  // Ajoutez un gestionnaire d'événements pour le clic sur le bouton "accept"
-  acceptButton.addEventListener('click', function() {
-    // Définir le cookie d'acceptation pour une durée de 30 jours
-    setCookie("cookiesAccepted", "true", 30);
-    // Masquer la boîte de dialogue
-    document.querySelector('.container').style.display = 'none';
-  });
-
-
+    window.axeptioSettings = {
+        clientId: "65e4b1b9c065d174cb3f24f4",
+        cookiesVersion: "carbonfight landing-fr-EU",
+      };
+       
+      (function(d, s) {
+        var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+        e.async = true; e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+      })(document, "script");
